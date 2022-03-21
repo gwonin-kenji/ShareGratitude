d43bd8cd3b8dc3f4d754743695fe56fb4db8f46f
gwonin-kenji <hangwonin@gmail.com>
2022-03-21 23:34:05 +0900

    add:user registration with plain-text password

---
+++ backend/routers/user.py
@@ -0,0 +1,25 @@
+from fastapi import status, HTTPException, Depends, APIRouter
+import schemas
+from models import User
+from db_drive import DB_Driver
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
+@router.post("/register", status_code=status.HTTP_201_CREATED, response_model=schemas.CreatedUserOut)
+def create_user(user: schemas.UserCreate, db: DB_Driver = Depends(DB_Driver)):
+
+    user_name = db.query_username(user.user_name)
+    if user_name:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"ユーザ名が重複しています")
+    user_email = db.query_email(user.user_email)
+    if user_email:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"メールアドレスが重複しています")
+
+    # TODO : パスワードはハッシュ化して保存
+    new_user = User(**user.dict())
+    db.add(new_user)
+    db.refresh_query(new_user)
+    return new_user

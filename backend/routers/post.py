d6148122aadbe1cd7a59e1d5c0bf757551a9662f
gwonin-kenji <hangwonin@gmail.com>
2022-03-24 00:06:53 +0900

    add:token system  to apply restrictions with a token

--- backend/routers/post.py
+++ backend/routers/post.py
@@ -1,6 +1,6 @@
 from typing import List
 from fastapi import Response, status, HTTPException, Depends, APIRouter
-import schemas
+import schemas, oauth2
 from models import Post
 from db_drive import DB_Driver
 
@@ -10,9 +10,12 @@ router = APIRouter(
 )
 
 
-# TODO : ポスト作成 初めは誰でも作成できるようにする。トークンでの制限は後程
 @router.post("/create", response_model=schemas.Post)
-def create_post(post: schemas.PostCreate, db: DB_Driver = Depends(DB_Driver)):
+def create_post(
+    post: schemas.PostCreate, 
+    db: DB_Driver = Depends(DB_Driver), 
+    current_user = Depends(oauth2.get_current_user),
+    ):
     # TODO : トークンでユーザを確認したら、感謝ポストの外部キーにユーザidを設定するようにupdateする処理をつける
     new_post = Post(**post.dict())
     db.add(new_post)
@@ -20,9 +23,13 @@ def create_post(post: schemas.PostCreate, db: DB_Driver = Depends(DB_Driver)):
     return new_post
 
 
-# TODO : トークンで制限をつける
 @router.put("/update/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: DB_Driver = Depends(DB_Driver)):
+def update_post(
+    id: int, 
+    updated_post: schemas.PostCreate, 
+    db: DB_Driver = Depends(DB_Driver),
+    current_user = Depends(oauth2.get_current_user),
+    ):
     post_query = db.query_post(id)
     post = post_query.first()
 
@@ -32,10 +39,13 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: DB_Driver = Depen
     db.commit()
     return post_query.first()
 
-
-# TODO : 削除
 @router.delete("/delete/{id}")
-def delete_post(id: int, db: DB_Driver = Depends(DB_Driver)):
+def delete_post(
+    id: int, 
+    db: DB_Driver = Depends(DB_Driver),
+    current_user = Depends(oauth2.get_current_user),    
+    ):
+    # TODO : ログインさえすれば誰でも削除できるので、トークンから取得できるユーザメールから、ユーザ名を取得し、外部キーが同じユーザ名の投稿を削除できるようにする
     post = db.query_post(id)
     if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with id: {id} is not found")
@@ -44,7 +54,6 @@ def delete_post(id: int, db: DB_Driver = Depends(DB_Driver)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
     
 
-# TODO : 表示
 @router.get("/", response_model=List[schemas.SharePost])
 def get_all_posts(db: DB_Driver = Depends(DB_Driver)):
     posts = db.query_all_posts()
@@ -52,6 +61,10 @@ def get_all_posts(db: DB_Driver = Depends(DB_Driver)):
 
 # TODO : トークンで制限をつけたとしてもトークンを持ったユーザが別のユーザのidを入力した場合、みることができるのではないか、、
 @router.get("/{id}")
-def get_user_post(id: int, db: DB_Driver = Depends(DB_Driver)):
+def get_user_post(
+    id: int, 
+    db: DB_Driver = Depends(DB_Driver),
+    current_user = Depends(oauth2.get_current_user),    
+    ):
     # 現在はパスにidを入力している仕様だが、トークンで制限をつけた後は、そのユーザのidに紐づいているポストを表示するって形で良さそうだ。
     db.query_user()
